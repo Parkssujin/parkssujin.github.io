e05b3e6f0d2ebfce5eb2e24d74bf6fd66ddf140e
Parkssujin <96673950+Parkssujin@users.noreply.github.com>
2024-04-13 18:04:07 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -10,9 +10,19 @@ document.addEventListener('scroll', () => {
 });
 // function(){} 이름이 없는 함수이기 때문에 화살표함수로 대체 () => {} 로 대체
 
-//Hone 섹션을 아래로 스크롤시 투명하게 처리함
+//Home 섹션을 아래로 스크롤시 투명하게 처리함
  const home = document.querySelector('.home__container');
  const homeHeight = home.offsetHeight;
  document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+ });
+
+ //Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight /2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
  });
